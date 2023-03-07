5542bd37f413ef9137df2487d5faf212cda18dbe
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-03-07 14:43:27 -0300

    Add xterm-addon-webgl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import "./style.css";
 import "xterm/css/xterm.css";
 import { Terminal } from "xterm";
-import { WebContainer } from "@webcontainer/api";
 import { FitAddon } from "xterm-addon-fit";
 import { WebLinksAddon } from "xterm-addon-web-links";
+import { WebglAddon } from "xterm-addon-webgl";
+import { WebContainer } from "@webcontainer/api";
 
 const terminalElement = document.querySelector<HTMLDivElement>("#terminal")!;
 const terminal = new Terminal({
@@ -11,11 +12,15 @@ const terminal = new Terminal({
   cursorBlink: true,
   tabStopWidth: 2,
 });
+
 const fitAddon = new FitAddon();
 const webLinksAddon = new WebLinksAddon();
+const webglAddon = new WebglAddon();
 
 terminal.loadAddon(fitAddon);
 terminal.loadAddon(webLinksAddon);
+terminal.loadAddon(webglAddon);
+
 terminal.open(terminalElement);
 fitAddon.fit();
 
