272cada951c852523856589abea9d79eb349855a
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-03-07 15:04:44 -0300

    Add window load event listener

--- src/main.ts
+++ src/main.ts
@@ -6,50 +6,52 @@ import { WebLinksAddon } from "xterm-addon-web-links";
 import { WebglAddon } from "xterm-addon-webgl";
 import { WebContainer } from "@webcontainer/api";
 
-const terminalElement = document.querySelector<HTMLDivElement>("#terminal")!;
-const terminal = new Terminal({
-  convertEol: true,
-  cursorBlink: true,
-  tabStopWidth: 2,
-});
-
-const fitAddon = new FitAddon();
-const webLinksAddon = new WebLinksAddon();
-const webglAddon = new WebglAddon();
-
-terminal.loadAddon(fitAddon);
-terminal.loadAddon(webLinksAddon);
-terminal.loadAddon(webglAddon);
-
-terminal.open(terminalElement);
-fitAddon.fit();
-
-const webContainer = await WebContainer.boot();
-const shell = await webContainer.spawn("jsh", {
-  terminal: {
-    cols: terminal.cols,
-    rows: terminal.rows,
-  },
-});
-
-window.addEventListener("resize", () => {
-  fitAddon.fit();
-  shell.resize({
-    cols: terminal.cols,
-    rows: terminal.rows,
+window.addEventListener("load", async () => {
+  const terminalElement = document.querySelector<HTMLDivElement>("#terminal")!;
+  const terminal = new Terminal({
+    convertEol: true,
+    cursorBlink: true,
+    tabStopWidth: 2,
   });
-});
 
-shell.output.pipeTo(
-  new WritableStream({
-    write(data) {
-      terminal.write(data);
+  const fitAddon = new FitAddon();
+  const webLinksAddon = new WebLinksAddon();
+  const webglAddon = new WebglAddon();
+
+  terminal.loadAddon(fitAddon);
+  terminal.loadAddon(webLinksAddon);
+  terminal.loadAddon(webglAddon);
+
+  terminal.open(terminalElement);
+  fitAddon.fit();
+
+  const webContainer = await WebContainer.boot();
+  const shell = await webContainer.spawn("jsh", {
+    terminal: {
+      cols: terminal.cols,
+      rows: terminal.rows,
     },
-  })
-);
+  });
 
-const input = shell.input.getWriter();
+  window.addEventListener("resize", () => {
+    fitAddon.fit();
+    shell.resize({
+      cols: terminal.cols,
+      rows: terminal.rows,
+    });
+  });
 
-terminal.onData(data => {
-  input.write(data);
+  shell.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        terminal.write(data);
+      },
+    })
+  );
+
+  const input = shell.input.getWriter();
+
+  terminal.onData(data => {
+    input.write(data);
+  });
 });
